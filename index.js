a6ec1c080d16fa6d882703a117e431818cd88ffd
Shain-Carrasco <shaincarrasco@gmail.com>
2020-02-10 23:30:06 -0800

    complete initial proxy with static assets hosted by S3

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 4001;
+const path = require('path')
+const proxy = require('http-proxy-middleware');
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.static(path.join(__dirname, '/client')));
+// app.use('/getUserById/101', proxy({ target: 'http://localhost:4000', changeOrigin: true}));
+
+
+app.listen(port, () => { console.log(`Proxy-Server is running on ${port}`); });
+
